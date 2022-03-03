d0e9cf3c35cd98fe25990c30e79022040e858fc6
Evgeniy <46823312+Evgenie@users.noreply.github.com>
2022-03-03 14:57:09 +0300

    fix(P-1): removed redundant files

--- src/theme/containers/Header/CTheme.ts
+++ src/theme/containers/Header/CTheme.ts
@@ -3,8 +3,6 @@ import cwrap from "./cwrap";
 import logo from "./logo";
 import button from "./button";
 import termin from "./termin";
-import navbar from "./navbar";
-import linknav from "./linknav";
 
 export interface ICTheme {
     cwrap: typeof cwrap;
@@ -12,8 +10,6 @@ export interface ICTheme {
     menu: typeof menu;
     logo: typeof logo;
     termin: typeof termin;
-    navbar: typeof navbar;
-    linknav: typeof linknav;
 }
 
 export const CTheme: ICTheme = {
@@ -22,8 +18,6 @@ export const CTheme: ICTheme = {
     menu,
     logo,
     termin,
-    navbar,
-    linknav,
 };
 
 export default CTheme;
